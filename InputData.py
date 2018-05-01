6d74d591c70e883771d917d48326d0dd5edd29d2
jenngilb <35672519+jenngilb@users.noreply.github.com>
2018-05-01 13:16:49 -0400

    test5

--- InputData.py
+++ InputData.py
@@ -23,8 +23,9 @@ TRANS_MATRIX = [
 # annual cost of each health state
 ANNUAL_STATE_COST = [
     34316.0,   # Healthy with HIV - Annual medical cost estimates for HIV-infected persons, adjusted for age, sex, race/ethnicity, and transmission risk group, were from the HIV Research Network (range $1,854–$4,545/month). We took the average - $3618 - and multiplied by 12 months for $43416 - source: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4359630/
-    37892.6,   # Experienced mild cardiac event - for patients with Medicaid, this is an incremental $4959 additional cost. For those with other insurance than Medicaid, there is an incremental $2655 cost. Because about 40% of people with HIV use Medicaid, we solved for this cost as follows (.6*2655+.4*4959)
-    75785.20   #Experienced severe cardiac event - we estimated this would be twice the cost of a mild cardiac event
+    37892.6,   # Experienced mild cardiac event - for patients with Medicaid, this is an incremental $4959 additional cost. For those with other insurance than Medicaid, there is an incremental $2655 cost. Because about 40% of people with HIV use Medicaid, we solved for this cost as follows (.6*2655+.4*4959) = 3576.6
+    48618.4,    #Experienced severe cardiac event - we estimated this would be four times the cost of a mild cardiac event (4*3575.6+34316.0)
+    77679.83   # Fatal cardiac event - data shows this costs 1.2-2.9 times more than a nonfatal cardiac event. We took the average and multipled the cost of a cardiac event above by 2.05. source: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4467662/
     ]
 #Source for data on cost:  https://www.ncbi.nlm.nih.gov/pubmed/28933204 - limitation: didn't account for fact that cost for elderly patients may be higher
 #Source for data on HIV pop with Medicaid as insurance: https://www.kff.org/hivaids/fact-sheet/medicaid-and-hiv/
@@ -36,7 +37,8 @@ ANNUAL_STATE_COST = [
 ANNUAL_STATE_UTILITY = [
     0.9,   # HIV+ and otherwise healthy
     0.7,   # Mild cardiac event
-    0.3 # Severe cardiac event
+    0.3, # Severe cardiac event
+    0.00001 #fatal cardiac event
     ]
 
 # annual cost of therapy versus untreated
